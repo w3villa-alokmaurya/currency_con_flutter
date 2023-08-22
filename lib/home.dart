f083f85775221f47f9409f0f1f8a3af2f96ed3a7
Alok Maurya <alok.maurya@w3villa.com>
2023-08-22 12:48:36 +0530

    Buttons added

--- lib/home.dart
+++ lib/home.dart
@@ -11,13 +11,13 @@ class Home extends StatelessWidget {
       ),
       borderSide: BorderSide(width: 2.0, style: BorderStyle.solid),
     );
-    return const Scaffold(
-      backgroundColor: Colors.deepPurpleAccent,
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 251, 87, 37),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               "0",
               style: TextStyle(
                 fontWeight: FontWeight.w700,
@@ -25,14 +25,14 @@ class Home extends StatelessWidget {
                 color: Color.fromRGBO(255, 255, 255, 1),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
+            const Padding(
+              padding: EdgeInsets.all(10.0),
               child: TextField(
                 style: TextStyle(
                   color: Colors.black,
                 ),
                 decoration: InputDecoration(
-                  hintText: 'Please Enter the amont in INR',
+                  hintText: 'Please Enter the amont in USD',
                   focusedBorder: border,
                   enabledBorder: border,
                   hintStyle: TextStyle(
@@ -46,6 +46,43 @@ class Home extends StatelessWidget {
                 keyboardType: TextInputType.numberWithOptions(
                   decimal: true,
                 ),
+              ),
+            ),
+            // Padding(
+            //   padding: const EdgeInsets.only(left: 10, right: 10),
+            //   child: TextButton(
+            //     onPressed: () {
+            //       //
+            //     },
+            //     style: const ButtonStyle(
+            //       backgroundColor: MaterialStatePropertyAll(Colors.black),
+            //       foregroundColor: MaterialStatePropertyAll(Colors.white),
+            //       minimumSize:
+            //           MaterialStatePropertyAll(Size(double.infinity, 50)),
+            //     ),
+            //     child: const Text('Click me'),
+            //   ),
+            // ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: ElevatedButton(
+                onPressed: () {
+                  //
+                },
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.purple),
+                  foregroundColor: MaterialStatePropertyAll(Colors.white),
+                  minimumSize:
+                      MaterialStatePropertyAll(Size(double.infinity, 50)),
+                  shape: MaterialStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+                child: const Text('Elevated'),
               ),
             ),
           ],
